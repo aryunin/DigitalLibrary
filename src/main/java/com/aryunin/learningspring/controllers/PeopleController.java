8df22103a7068a451724338f6bfcefad63d63c3e
aryunin <im.aryunin@gmail.com>
2022-07-31 16:42:58 +0300

    Added new person mapping

--- src/main/java/com/aryunin/learningspring/controllers/PeopleController.java
+++ src/main/java/com/aryunin/learningspring/controllers/PeopleController.java
@@ -1,25 +1,47 @@
 package com.aryunin.learningspring.controllers;
 
 import com.aryunin.learningspring.dao.PersonDAO;
+import com.aryunin.learningspring.models.Person;
+import com.aryunin.learningspring.util.PersonValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
     final private PersonDAO personDAO;
+    final private PersonValidator personValidator;
 
     @Autowired
-    public PeopleController(PersonDAO personDAO) {
+    public PeopleController(PersonDAO personDAO, PersonValidator personValidator) {
         this.personDAO = personDAO;
+        this.personValidator = personValidator;
     }
 
     @GetMapping
     public String index(Model model) {
         model.addAttribute("people", personDAO.getAllPeople());
         return "people/index";
+    }
+
+    @GetMapping("/new")
+    public String newPersonForm(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+        personValidator.validate(person, bindingResult);
+        if(bindingResult.hasErrors()) return "people/new";
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
