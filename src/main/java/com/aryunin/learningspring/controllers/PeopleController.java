cf21327c3ce51ef8e491ac8bc784aa8e70be81c8
aryunin <im.aryunin@gmail.com>
2022-07-31 15:18:44 +0300

    Added people & books controllers

---
+++ src/main/java/com/aryunin/learningspring/controllers/PeopleController.java
@@ -0,0 +1,9 @@
+package com.aryunin.learningspring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+}
