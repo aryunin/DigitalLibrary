e2f5f3b08191069c973c9a1a2bb390fd5e5628d4
aryunin <im.aryunin@gmail.com>
2022-07-31 15:51:32 +0300

    Added method getAllPeople to PersonDAO

--- src/main/java/com/aryunin/learningspring/dao/PersonDAO.java
+++ src/main/java/com/aryunin/learningspring/dao/PersonDAO.java
@@ -1,7 +1,23 @@
 package com.aryunin.learningspring.dao;
 
+import com.aryunin.learningspring.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class PersonDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> getAllPeople() {
+        return jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));
+    }
 }
