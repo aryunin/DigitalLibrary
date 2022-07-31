337e9f46bc900928a7b47df2aa89b9fe9de0d08f
aryunin <im.aryunin@gmail.com>
2022-07-31 15:52:17 +0300

    Added index mapping for PeopleController

--- src/main/java/com/aryunin/learningspring/controllers/PeopleController.java
+++ src/main/java/com/aryunin/learningspring/controllers/PeopleController.java
@@ -1,9 +1,25 @@
 package com.aryunin.learningspring.controllers;
 
+import com.aryunin.learningspring.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
+    final private PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("people", personDAO.getAllPeople());
+        return "people/index";
+    }
 }
